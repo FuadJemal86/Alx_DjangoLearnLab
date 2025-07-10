c14a5c68153471f851b060261ce3f9f439cbd83e
Fuad Jemal <fuad47722@gmail.com>
2025-07-10 18:17:16 +0300

    refactor the component

--- Introduction_to_Django/LibraryProject/LibraryProject/README.md
+++
@@ -1,3 +0,0 @@
-# LibraryProject
-
-This is a beginner Django project created as part of the Introduction_to_Django lab.
--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8!2nacpqvvayrm4ezlto(8kwh+%gyn6-%5a0+oai&3yka)xcl4'
+SECRET_KEY = 'django-insecure-g1yo+q=6%u-xl=q6m(#1kh%*5$y2lmj-p6uf&r7zh%g764vso@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
