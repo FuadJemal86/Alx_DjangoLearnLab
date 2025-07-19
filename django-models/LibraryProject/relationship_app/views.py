478d9147066284b01e093af05a80a7b4e6ec44cf
Fuad Jemal <fuad47722@gmail.com>
2025-07-19 08:57:42 +0300

    correct 2 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,15 @@
 # relationship_app/views.py
 from django.shortcuts import render
-from .models import Book
-# relationship_app/views.py
-from django.views.generic import DetailView
-from .models import Library
+from django.views.generic.detail import DetailView  # Correct import for DetailView
+from .models import Book, Library
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()  # Retrieve all books from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'  # Name of the context variable to be passed to the template
+    template_name = 'relationship_app/library_detail.html'  # Specify the template for rendering
+    context_object_name = 'library'  # The name of the context variable passed to the template
