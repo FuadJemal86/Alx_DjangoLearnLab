48bcb9adb5006a1db7af87b83ff00fe2a89f4a32
Fuad Jemal <fuad47722@gmail.com>
2025-07-20 08:30:24 +0300

    make correct

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,33 +1,26 @@
-# relationship_app/views.py
-from django.shortcuts import render
-from .models import Book
-# relationship_app/views.py
-from django.views.generic.detail import DetailView
-from .models import Library
 from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from .models import Book, Library
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    return render(request, '/relationship_app/list_books.html', {'books': books})
-
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = '/relationship_app/library_detail.html'
-    context_object_name = 'library'  # Name of the context variable to be passed to the template
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
-
-
-
+# User registration view
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login') 
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
