f3f1ed2a9ffbb28988daef4808899cc99ea853c6
Fuad Jemal <fuad47722@gmail.com>
2025-07-20 08:01:31 +0300

    we make for register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from .models import Book
 # relationship_app/views.py
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Function-based view to list all books
 def list_books(request):
@@ -16,3 +18,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = '/relationship_app/library_detail.html'
     context_object_name = 'library'  # Name of the context variable to be passed to the template
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
