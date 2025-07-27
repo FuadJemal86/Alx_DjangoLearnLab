a0a65c4e0b3d6087fae09ef23a8b328bbbf520f6
Fuad Jemal <fuad47722@gmail.com>
2025-07-27 08:37:42 +0300

    add book list

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
 
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
     books = Book.objects.all()
