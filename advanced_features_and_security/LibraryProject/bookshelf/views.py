4be672bb304e0b829621993571b3f7083cf1fd2c
Fuad Jemal <fuad47722@gmail.com>
2025-07-27 10:30:59 +0300

    task 2

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -29,3 +29,17 @@ def edit_book(request, pk):
 def delete_book(request, pk):
     # delete logic
     pass
+
+
+
+from django.shortcuts import render
+from .models import Book
+from .forms import BookSearchForm
+
+def search_books(request):
+    form = BookSearchForm(request.GET or None)
+    books = Book.objects.none()
+    if form.is_valid():
+        title = form.cleaned_data['title']
+        books = Book.objects.filter(title__icontains=title)
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
