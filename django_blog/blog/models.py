b8f3bb3dd22abf52cd08f88dc270039df222099d
Fuad Jemal <fuad47722@gmail.com>
2025-08-11 17:07:11 +0300

    correct the model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)    
+# class User(models.Model):
+#     name = models.CharField(max_length=200)
+#     author = models.CharField(max_length=200)    
     
     
 class Post(models.Model):
