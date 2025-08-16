cabe4989080696cff2e20dec246deab585881908
Fuad Jemal <fuad47722@gmail.com>
2025-08-16 10:19:25 +0300

    add the missing urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', views.search_posts, name='search-posts'),
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
+    path('search/', views.search_posts, name='search-posts'),
+    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
 ]
