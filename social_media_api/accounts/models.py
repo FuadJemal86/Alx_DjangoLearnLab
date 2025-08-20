ff5ca3168cd1f3d7596606ba03a6edbe71d403bb
Fuad Jemal <fuad47722@gmail.com>
2025-08-20 11:28:17 +0300

    add model text field

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    bio = models.CharField(max_length=200, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField(
         "self",
