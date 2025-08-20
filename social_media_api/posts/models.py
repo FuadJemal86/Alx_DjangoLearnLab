c0d679d753982ff0086f6c0281fc8002785ee1de
Fuad Jemal <fuad47722@gmail.com>
2025-08-20 12:39:22 +0300

    upgrade task 2

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,15 +1,24 @@
 from django.db import models
+from django.conf import settings  # to get the custom user model
 
-# Create your models here.
+# Use settings.AUTH_USER_MODEL to reference the User model
 class Post(models.Model):
-    title = models.TextField(max_length= 100)
-    content = models.TextField(max_length=100) 
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.TextField(max_length=200, blank=True)
+    content = models.TextField(max_length=200, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
