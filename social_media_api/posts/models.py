a97b5e51cc09e0bec03b36c9ce46222971f140b8
Fuad Jemal <fuad47722@gmail.com>
2025-08-20 12:31:56 +0300

    add text field in the model

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length= 100)
-    content = models.CharField(max_length=100) 
+    title = models.TextField(max_length= 100)
+    content = models.TextField(max_length=100) 
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
