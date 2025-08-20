24a31563e90a65c14111527570a5bee83c5248b5
Fuad Jemal <fuad47722@gmail.com>
2025-08-20 12:29:23 +0300

    create posts app

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -15,15 +15,3 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
     
-class Post(models.Model):
-    title = models.CharField(max_length= 100)
-    content = models.CharField(max_length=100) 
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
