42c55598ac4cf3bca70e2c1820db27da1796b3b7
Fuad Jemal <fuad47722@gmail.com>
2025-08-22 08:22:34 +0300

    add feed in the post urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -6,6 +6,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
+router.register(r'feed', FeedView)
 
 urlpatterns = [
     path('', include(router.urls)),
