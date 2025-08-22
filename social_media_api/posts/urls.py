9a92d067fb530470d4ea3951a21177d3fd8e186d
Fuad Jemal <fuad47722@gmail.com>
2025-08-22 08:20:30 +0300

    add as view in the urls part

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 # accounts/urls.py
 from django.urls import path , include
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet , FeedView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -8,6 +8,6 @@ router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    
     path('', include(router.urls)),
+    path('feed/' , FeedView.as_view() , name='feed')
 ]
