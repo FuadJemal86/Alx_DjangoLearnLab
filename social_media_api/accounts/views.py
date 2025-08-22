0623baf2ec5e1bc9352feb1a174fb8b33e7016c0
Fuad Jemal <fuad47722@gmail.com>
2025-08-22 07:46:48 +0300

    add query set

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -85,6 +85,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)    
 
 class FollowerUserViewSet(generics.GenericAPIView):
+     queryset = User.objects.all()
      permission_classes = [permissions.IsAuthenticated]
 
      def post(self, request, user_id):
@@ -99,6 +100,7 @@ class FollowerUserViewSet(generics.GenericAPIView):
 
 
 class UnFollowUserViewSet(generics.GenericAPIView):
+     queryset = User.objects.all()
      permission_classes = [permissions.IsAuthenticated]
 
      def post(self, request, user_id):
